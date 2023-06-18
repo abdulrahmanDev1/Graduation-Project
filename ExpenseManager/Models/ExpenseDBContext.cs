2263b8b26539e86898d0b5ffa9b404260149c201
Abdulrhman Mohammed <61534551+abdulrhmanmohammed198@users.noreply.github.com>
2023-06-18 16:51:45 +0300

    initial commit

---
+++ ExpenseManager/Models/ExpenseDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseManager.Models
+{
+    public class ExpenseDBContext : DbContext
+    {
+        public ExpenseDBContext(DbContextOptions<ExpenseDBContext> options) : base(options)
+        {
+
+        }
+        public virtual DbSet<ExpenseReport> ExpenseReport { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+                optionsBuilder.UseSqlServer("Data Source =D7OM; Initial Catalog =Expensses_Manager; Persist Security Info = True; Integrated Security = true");
+            }
+        }
+    }
+}
---
+++ ExpenseManager/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+const scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 1000,
+    speedAsDuration: true
+});
